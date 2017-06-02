1ec8fbc1fc52108943e47ece28233d25df466216
HenriqueCRodrigues <henrique.c.rodrigues@hotmail.com>
2017-06-02 02:08:59 -0300

    CRUD do Usuario Pré pronto

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -41,11 +41,41 @@ class AuthController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
+        return Validator::make($data, 
+            [
+            'name'                  => 'required|max:255',
+            'email'                 => 'required|email|max:100|unique:users',
+            'login'                 => 'required|max:30|unique:users'
+            'password'              => 'required|confirmed|min:4',
+            'password_confirmation' => 'required|min:6',
+            ],
+            
+
+            [
+            'name.required'                   => 'É obrigatório inserir o Nome',
+            'name.max'                        => 'O nome deve ter no maximo :max caracteres',
+            
+            'email.required'                  => 'É obrigatório inserir o Email.',
+            'email.email'                     => 'Inserir formato de email correto, EX: exemplo@domain.com.',
+            'email.max'                       => 'O Email deve ter no maximo :max caracteres.',
+            'email.unique'                    => 'Não pode haver Email Iguais, digite outro por favor.', 
+
+            'login.required'                  => 'É obrigatório inserir o Login',
+            'login.max'                       => 'O Login deve ter no maximo :max caracteres',
+            'login.unique'                    => 'Não pode haver Logins Iguais, digite outro por favor.',
+            
+            'password.required'               => 'É obrigatório inserir a Senha',
+            'password.confirmed'              => 'O campo Senha esta com valor diferente do campo Confirmar Senha.',
+            'passaword.min'                   => 'Digitar ao menos :min caracteres para a sua senha',
+
+            'password_confirmation.required'  => 'É obrigatório inserir a Confirmação de Senha.',
+            'password_confirmation.confirmed' => 'O campo Confirmar Senha esta com valor diferente do campo Senha.',
+            'passaword_confirmation.min'      => 'Digitar ao menos :min caracteres para a sua senha.',
+            
+            ],
+
+
+            );
     }
 
     /**
@@ -59,6 +89,7 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'login' => $data['login'],
             'password' => bcrypt($data['password']),
         ]);
     }
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('PASTALYOUT.ARQUIVOLAYOUT');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        if(Auth::user()->id == $id) 
+        {
+        $user->name     = $request->name;
+        $user->email    = $request->email;
+        $user->login    = $request->login;
+        $user->password = $request->password;
+
+        $user->save();
+        }
+
+        return redirect('PREFIX DA ROTA');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        
+        if (Auth::user()->id == $id) 
+        {
+            $user->delete();
+        }
+
+        return redirect('PAGINA INICIAL');
+    }
+}
